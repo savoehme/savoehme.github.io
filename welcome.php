2bbc0ed73660c9e30151c0a7e98f9803bef19bb1
savoehme <80652966+savoehme@users.noreply.github.com>
2021-04-17 19:40:00 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+Category is: <?php echo $_post["photography, design, other"]; ?>
+message: <?php echo $_post["message"];?>
+
+</body>
+</html> 
